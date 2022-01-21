fd012816cc0aca63ab5e79662ce9493a3cce12c1
D.Koryakovsky <d.koryakovsky@op.local>
2022-01-21 10:56:54 +0300

    Add fileName in the first parameter

--- src/ru/avalon/javapp/devj130/FileSender.java
+++ src/ru/avalon/javapp/devj130/FileSender.java
@@ -7,15 +7,21 @@ public class FileSender {
     private static final String HOST = "localhost";
 
     public static void main(String[] args) {
+
+        if (args.length < 1) {
+            System.out.println("Please specify the file name as the first parameter");
+            System.exit(1);
+        }
+
         System.out.println("File sender started...");
-        new FileSender().run(HOST);
+        new FileSender().run(HOST, args[0]);
         System.out.println("File sender finished.");
     }
 
-    private void run(String ip) {
+    private void run(String ip, String fileName) {
         try(Socket sock = new Socket(ip, FileReceiver.SERVER_PORT);
             OutputStream out = sock.getOutputStream()) {
-            File file = new File("E:\\test.docx");
+            File file = new File(fileName);
             byte[] buf = new byte[FileReceiver.BUFFER_SIZE];
             byte[] bufFileName = file.getName().getBytes();
             out.write(bufFileName);
